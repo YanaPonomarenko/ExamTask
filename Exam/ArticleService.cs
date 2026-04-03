4c4c4ce87dd7c1bff694e1cb5b37549a622e317f
Yana Ponomarenko <y.ponomar05@gmail.com>
2026-04-03 20:53:52 +0300

    added files

---
+++ Exam/ArticleService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+namespace Exam;
+
+public class ArticleService
+{
+    private readonly HttpClient _client;
+    private readonly string _baseUrl;
+
+    public ArticleService(string baseUrl)
+    {
+        _client = new HttpClient();
+        _baseUrl = baseUrl;
+    }
+
+    public async Task<List<Article>> GetAllAsync() =>
+        await _client.GetFromJsonAsync<List<Article>>(_baseUrl);
+
+    public async Task<Article> GetByIdAsync(string id) =>
+        await _client.GetFromJsonAsync<Article>($"{_baseUrl}/{id}");
+
+    public async Task<List<Article>> SearchByTitleAsync(string title) =>
+        await _client.GetFromJsonAsync<List<Article>>($"{_baseUrl}?title={title}");
+
+    public async Task<string?> AddArticleAsync(Article article)
+    {
+        var response = await _client.PostAsJsonAsync(_baseUrl, article);
+
+        if (response.IsSuccessStatusCode)
+        {
+            var added = await response.Content.ReadFromJsonAsync<Article>();
+            return added?.id;
+        }
+        return null;
+    }
+}
---
+++ Exam/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Exam
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var service = new ArticleService("http://localhost:3000/articles");
+
+            var newArticle = new Article
+            {
+                Title = "Test2",
+                Description = "www",
+                Author = "Alex",
+                Date = DateTime.Now
+            };
+
+            string? newId = await service.AddArticleAsync(newArticle);
+            Console.WriteLine($"Added new articles with ID = {newId}");
+
+            var all = await service.GetAllAsync();
+            Console.WriteLine("\nAll articles:");
+            all.ForEach(a => Console.WriteLine($"ID: {a.id}, Title: {a.Title}"));
+
+            var search = await service.SearchByTitleAsync("Test");
+            Console.WriteLine("\nResult:");
+            search.ForEach(a => Console.WriteLine($"ID: {a.id}, Title: {a.Title}"));
+        }
+    }
+}
